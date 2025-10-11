333001a669af02c4a610744a0ab98392d74351b9
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2025-10-11 10:16:50 -0300

    Enhance dashboard layout with user greeting and property cards

--- dashboard.php
+++ dashboard.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link href='https://cdn.boxicons.com/fonts/basic/boxicons.min.css' rel='stylesheet'>
 </head>
 
 <body class="flex w-screen h-screen">
@@ -49,6 +50,89 @@
             <p>Icon</p>
         </div>
     </div>
+
+    <!-- Main content -->
+    <main class="flex flex-col w-full h-full bg-gray-300 px-8 py-4 gap-8">
+        <div class="flex flex-col gap-4">
+            <div class="flex gap-4 items-center ">
+                <div class="p-4 rounded-full bg-blue-600 text-white">
+                    <i class='bx  bx-user text-[54px]'></i>
+                </div>
+                <div class="flex-col gap-4">
+                    <h3 class="text-2xl font-semibold">Hola, Nicolas Chiarello</h3>
+                    <p>Administra tus inmuebles aqui</p>
+                </div>
+            </div>
+            <button>
+                <div class="flex gap-4 text-white rounded-lg bg-blue-600 w-fit px-4 py-2">
+                    <i class='bxr  bx-plus text-2xl'></i>
+                    <span>Agregar inmueble</span>
+                </div>
+            </button>
+        </div>
+        <!-- Property list -->
+        <div
+            class="flex w-full h-full overflow-y-scroll bg-gray-600 rounded-lg scrollbar-none [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+            <!-- Propery card -->
+            <div class="w-[380px] h-[215px]  bg-blue-400 rounded-2xl shadow-lg relative overflow-hidden">
+                <!-- Image -->
+                <div class="w-full h-full">
+                    <img src="https://picsum.photos/800" alt="Property Image" class="w-full h-full object-cover">
+                </div>
+
+                <!-- Overlay -->
+                <div class="absolute inset-0 bg-black/60 pointer-events-none"></div>
+
+                <!-- Content -->
+                <div class="w-full h-full absolute inset-0 flex flex-col justify-between p-2">
+                    <div class="flex justify-between">
+                        <div class="px-3 py-1 text-sm bg-blue-600 rounded-lg text-white">
+                            <p>Alquiler</p>
+                        </div>
+                        <div class="flex gap-2">
+                            <div class="flex gap-1 items-center text-white ">
+                                <p class="font-semibold">1</p>
+                                <i class='bx bx-bed text-xl'></i>
+                            </div>
+                            <div class="flex gap-1 items-center text-white ">
+                                <p class="font-semibold">2</p>
+                                <i class='bx bx-bath text-xl'></i>
+                            </div>
+                            <div class="flex gap-1 items-center text-white ">
+                                <i class='bx bx-garage text-xl'></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex justify-between text-white ">
+                        <div class="flex flex-col gap-1 justify-end">
+                            <div class="flex gap-2">
+                                <i class='bx bx-location-blank text-lg'></i>
+                                <p class="text-xs font-semibold">Maipu</p>
+                            </div>
+                            <div class="flex gap-2">
+                                <i class='bx bx-ruler text-lg'></i>
+                                <p class="text-xs font-semibold">120 M2</p>
+                            </div>
+                            <div class="flex gap-2">
+                                <p class="text-xs font-semibold">USD</p>
+                                <p class="text-xs font-semibold"> $120.000
+                                </p>
+                            </div>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <button class="flex items-center justify-center p-2 bg-red-600 rounded-lg text-white">
+                                <i class="bx bx-trash text-xl"></i>
+                            </button>
+                            <button class="flex items-center justify-center p-2 bg-blue-600 rounded-lg text-white">
+                                <i class="bx bx-edit text-xl"></i>
+                            </button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
 
 </html>
