a561b5862b99b88c1003a29412c1b8c8b500d713
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2025-10-25 09:40:20 -0300

    fix: update navbar structure to use anchor tag for logo link

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,10 +1,10 @@
 <div class="w-full py-4 bg-primary flex items-center justify-between px-8">
-    <div class="flex gap-2 items-center">
+    <a href="index.php" class="flex gap-2 items-center">
         <div class="w-8 h-8">
             <?php include './public/logo.svg'; ?>
         </div>
         <h1 class="hidden sm:block font-semibold text-2xl text-white">AlquiloApp</h1>
-    </div>
+    </a>
     <?php
     session_start();
     if (isset($_SESSION['user_id'])) {
