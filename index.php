f9c502603e3b61595229d16b4e7d1211c89903f5
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2025-10-26 10:16:27 -0300

    fix: remove commented Tailwind CSS script for cleaner code

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- <script src="https://cdn.tailwindcss.com"></script> -->
   <link rel="stylesheet" href="./styles/output.css">
   <link rel="stylesheet" href="https://cdn.boxicons.com/fonts/basic/boxicons.min.css">
   <title>AlquiloApp</title>
